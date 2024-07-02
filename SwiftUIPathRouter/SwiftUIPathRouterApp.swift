4a4a40d3aeb851018dbe331de5aca7365397dad5
Mellong <121382647@qq.com>
2024-07-02 14:01:44 +0800

    Initial Commit

---
+++ SwiftUIPathRouter/SwiftUIPathRouterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPathRouterApp.swift
+//  SwiftUIPathRouter
+//
+//  Created by mellong on 2024/7/2.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPathRouterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
